b74a7b2fabc50597093f5c63872c275a25596315
p370365 <p370365@localdomain>
2016-09-29 12:04:22 +0200

    List and Show Views

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -21,34 +21,12 @@ plugin.tx_partnerlisting_partnerlisting {
 }
 
 plugin.tx_partnerlisting._CSS_DEFAULT_STYLE (
-    textarea.f3-form-error {
-        background-color:#FF9F9F;
-        border: 1px #FF0000 solid;
-    }
-
-    input.f3-form-error {
-        background-color:#FF9F9F;
-        border: 1px #FF0000 solid;
-    }
-
-    .tx-partner-listing table {
-        border-collapse:separate;
-        border-spacing:10px;
-    }
-
-    .tx-partner-listing table th {
-        font-weight:bold;
-    }
-
-    .tx-partner-listing table td {
-        vertical-align:top;
-    }
-
-    .typo3-messages .message-error {
-        color:red;
-    }
-
-    .typo3-messages .message-ok {
-        color:green;
-    }
+  #tx-partner-listing-microsite #info-image img,
+  #tx-partner-listing-microsite #service-image img,
+  #tx-partner-listing-microsite #contact-image img {
+    float: left;
+    width: 34%;
+    height: auto;
+    margin-right: 30px;
+  }
 )
